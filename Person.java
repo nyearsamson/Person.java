4c8f2557af9bf627afe996018141b40c1c9fead3
nyearsamson <79901487+nyearsamson@users.noreply.github.com>
2021-03-08 19:29:25 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,13 @@
+# Person.java 
+public class main {
+ String first name = "Nyear" ;
+ String first name = "Samson" ;
+ 
+ 
+public static void main (string [] args) {
+ main myObj = new Main ();
+ System.out.println("First Name:" + myObj.firstname);
+ System.out.println("First Name:" + myObj.lastname);
+ }
+ }
+ 
