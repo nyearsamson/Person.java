d203e1672385265fe8183fee71667e286d8d1e45
nyearsamson <79901487+nyearsamson@users.noreply.github.com>
2021-03-08 20:08:00 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class main{
+  private String firstname1= "Ike";
+  private String lastname1= "Rener";
+  private String firstname2= "Frea";
+  private String lastname2= "Diosana";
+  private String firstname3= "Jorros";
+  private String lastname3= "Suela";
+  private String firstname4= "Kyben";
+  private String lastname4= "Nabor";
+  private String firstname5= "Steven";
+  private String lastname5= "Ryan";
+  
+  public static void main (String [] args) {
+    Main myObj = new Main ();
+    System.out.println (Name: " + myObj.firstname1 + " " + myobj.lastname1);
+    System.out.println (Name: " + myObj.firstname2 + " " + myobj.lastname2);
+    System.out.println (Name: " + myObj.firstname3 + " " + myobj.lastname3);
+    System.out.println (Name: " + myObj.firstname4 + " " + myobj.lastname4);
+    System.out.println (Name: " + myObj.firstname5 + " " + myobj.lastname5);
+                        
+     }
+ }
