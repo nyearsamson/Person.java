c78e968ec99d46b1147f4dc1b4325a56e6f22d6f
nyearsamson <79901487+nyearsamson@users.noreply.github.com>
2021-03-08 19:48:53 +0800

    Create Person1.java
    Amazing

---
+++ Person1.java
@@ -0,0 +1,33 @@
+Public class Person
+  {
+  private String Firstname;
+  private String Lastname;
+  
+  public String getName(){
+  return Firstname;
+  }
+  public String getName(){
+  return Lastname;
+    
+    Public class Person
+  {
+  String Firstname;
+  String Lastname;
+       
+  public static void main (String[] args)
+  {
+   Person p= new person ();
+    
+  p.setFirstname ("Nyear");
+  p.setLastname ("Samson");
+  System.out.println(p.getFirstname());
+  System.out.println(p.getLastname());
+    
+  p.printDetails();
+  }
+  public void setFirstName(String Firstname)
+  {
+  this.Firstname=firstname; 
+  }
+  public void setLastName(String Lastname)
+  {
